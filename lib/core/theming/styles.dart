cfe68fb50aa3a8b3403e16e3a0c396141bf29ce0
s4r4h4mdy0x01 <sara101hamdy@gmail.com>
2024-07-11 14:06:41 +0300

    Add Font Weight & styles  app files

---
+++ lib/core/theming/styles.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:sakany/core/theming/colors.dart';
+import 'package:sakany/core/theming/font_weight.dart';
+
+class TextStyleanage {
+  static TextStyle font24BlackBold = TextStyle(
+    fontSize: 24.sp,
+    fontWeight: FontWeightManager.bold,
+    color: Colors.black,
+  );
+
+}
