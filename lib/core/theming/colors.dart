d9a8c1caeb735eff4e102ae4700dddafba59e51c
s4r4h4mdy0x01 <sara101hamdy@gmail.com>
2024-07-11 14:00:49 +0300

    Add Colors app file

---
+++ lib/core/theming/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ColorManager {
+  static  Color primary = const Color(0xff9897D0);
+  static  Color textPrimaryDark = const Color(0xff544778);
+  static Color grey = const Color(0xffA1A1A1);
+  static Color darkPrimary = const Color(0xff4D4D6A);
+  static Color white = const Color(0xffFFFFFF);
+  static Color error = const Color(0xffe61f34);
+  static Color rad = Colors.red;
+  static Color black = Colors.black;
+}
